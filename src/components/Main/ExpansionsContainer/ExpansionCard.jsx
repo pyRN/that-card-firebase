5a5ad769a731fe0e8b84db2fdb0d42103681f6f0
Jacob Mayeux <jacobmayeux@gmail.com>
2021-10-08 15:42:02 -0500

    Refactored and styled Expansions container

--- src/components/Main/ExpansionsContainer/ExpansionCard.jsx
+++ src/components/Main/ExpansionsContainer/ExpansionCard.jsx
@@ -10,35 +10,7 @@ export default function ExpansionCard({ oExpansionInfo }) {
   const handleOnClick = (event) => {
     event.preventDefault();
 
-    function getCardsFromExpansion(cards, currentURL) {
-      fDispatch({
-        type: "SET_LOADING",
-        payload: {
-          bIsDataLoading: true,
-        },
-      });
-
-      fetch(currentURL)
-        .then((response) => response.json())
-        .then((data) => {
-          cards = cards.concat(data.data);
-          if (data.has_more) {
-            getCardsFromExpansion(cards, data.next_page);
-          } else {
-            fDispatch({
-              type: "SET_SEARCH_RESULTS",
-              payload: {
-                sTitle: `Do I Have Cards From: ${oExpansionInfo.name}`,
-                bIsFromSet: true,
-                sInputValue: oExpansionInfo.code,
-                aDisplayedCards: cards,
-                bIsDataLoading: false,
-              },
-            });
-          }
-        });
-    }
-
+    //Get cards from expansions
     getCardsFromExpansion(
       [],
       `https://api.scryfall.com/cards/search?order=set&q=e%3A${oExpansionInfo.code}&unique=prints`
@@ -46,12 +18,30 @@ export default function ExpansionCard({ oExpansionInfo }) {
     fHistory.push("/cards");
   };
 
+  function getCardsFromExpansion(cards, currentURL) {
+    fetch(currentURL)
+      .then((response) => response.json())
+      .then((data) => {
+        cards = cards.concat(data.data);
+        if (data.has_more) {
+          getCardsFromExpansion(cards, data.next_page);
+        } else {
+          fDispatch({
+            type: "SET_CARDS_DISPLAYED",
+            payload: {
+              sTitle: `Do I Have Cards From: ${oExpansionInfo.name}`,
+              bIsFromSet: true,
+              sInputValue: oExpansionInfo.code,
+              aDisplayedCards: cards,
+              bIsDataLoading: false,
+            },
+          });
+        }
+      });
+  }
+
   return (
-    <div
-      className="expansion-card"
-      onClick={handleOnClick}
-      style={{ cursor: "pointer" }}
-    >
+    <div className="expansion-card" onClick={handleOnClick}>
       <div className="expansion-items" align="center">
         <img
           src={oExpansionInfo.icon_svg_uri}
