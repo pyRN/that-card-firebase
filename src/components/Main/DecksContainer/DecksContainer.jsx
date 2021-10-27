09675552e1dccccd44f80cc5248999992ead95a6
Jacob Mayeux <jacobmayeux@gmail.com>
2021-10-27 06:56:41 -0500

    Placed 'under construction' cards on pages

--- src/components/Main/DecksContainer/DecksContainer.jsx
+++ src/components/Main/DecksContainer/DecksContainer.jsx
@@ -1,3 +1,10 @@
 export default function DecksContainer() {
-  return <div>Decks Container</div>;
+  return (
+    <div className="page-container">
+      <div className="responsive-card">
+        <h2 className="card-title-text">Decks</h2>
+        <h5>Under Construction</h5>
+      </div>
+    </div>
+  );
 }
--- src/components/Main/ResourcesContainer/ResourcesContainer.jsx
+++ src/components/Main/ResourcesContainer/ResourcesContainer.jsx
@@ -1,3 +1,10 @@
 export default function ResourcesContainer() {
-  return <div>Resources Container</div>;
+  return (
+    <div className="page-container">
+      <div className="responsive-card">
+        <h2 className="card-title-text">Resources</h2>
+        <h5>Under Construction</h5>
+      </div>
+    </div>
+  );
 }
