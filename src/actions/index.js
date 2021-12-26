c48b2af862102c0bf001e7ae1cfe7504ec88f947
Jacob Mayeux <jacobmayeux@gmail.com>
2021-12-26 09:26:58 -0600

    Moved reusable dispatches to index for calling

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,10 @@
-import { SIGN_IN, SIGN_OUT } from "./types";
+import {
+  SET_CARDS_DISPLAYED,
+  SET_FILTERED_CARDS,
+  SET_IS_LOADING,
+  SIGN_IN,
+  SIGN_OUT,
+} from "./types";
 
 export const signIn = (oUser) => (dispatch) => {
   dispatch({
@@ -11,5 +17,39 @@ export const signOutUser = () => (dispatch) => {
   dispatch({
     type: SIGN_OUT,
     payload: null,
+  });
+};
+
+export const resetCardSearch = () => (dispatch) => {
+  dispatch({
+    type: SET_CARDS_DISPLAYED,
+    payload: {
+      aDisplayedCards: null,
+    },
+  });
+  dispatch({
+    type: SET_IS_LOADING,
+    payload: {
+      bIsLoading: true,
+    },
+  });
+  dispatch({
+    type: SET_FILTERED_CARDS,
+    payload: null,
+  });
+};
+
+export const setCardSearch = (aDisplayedCards, bIsLoading) => (dispatch) => {
+  dispatch({
+    type: SET_CARDS_DISPLAYED,
+    payload: {
+      aDisplayedCards: aDisplayedCards,
+    },
+  });
+  dispatch({
+    type: SET_IS_LOADING,
+    payload: {
+      bIsLoading: bIsLoading,
+    },
   });
 };
