1773dc093532622ba3e4015614e29f82839c2f51
Jacob Mayeux <jacobmayeux@gmail.com>
2022-02-01 06:59:22 -0600

    Moved card fetching code to centralized location for reuse, removed unneeded imports

--- src/components/Main/ExpansionsContainer/ExpansionCard.jsx
+++ src/components/Main/ExpansionsContainer/ExpansionCard.jsx
@@ -1,112 +1,19 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { setCardSearch, resetCardSearch } from "../../../actions/index";
-import {
-  getFirestore,
-  getDocs,
-  collection,
-  query,
-  where,
-} from "firebase/firestore";
+import { fnFetchCards } from "../../../actions/index";
 import "./ExpansionsContainer.css";
 
 export default function ExpansionCard({ oExpansionInfo }) {
   const oUser = useSelector((state) => state.oUserReducer.oUser);
-  const db = getFirestore();
   const fnDispatch = useDispatch();
   const fnHistory = useHistory();
 
-  const handleOnClick = (event) => {
-    event.preventDefault();
-    fnDispatch(resetCardSearch());
+  const handleOnClick = (oEvent) => {
+    oEvent.preventDefault();
+    fnDispatch(fnFetchCards(oExpansionInfo.code, oUser, false, true));
 
-    new Promise((fnResolveExpansions, fnReject) => {
-      //Get cards from expansions
-      getCardsFromExpansion(
-        [],
-        `https://api.scryfall.com/cards/search?order=set&q=e%3A${oExpansionInfo.code}&unique=prints`,
-        fnResolveExpansions
-      );
-    }).then((aCards) => {
-      let aCardIds = [];
-
-      aCards.forEach((oCard) => {
-        aCardIds.push(oCard.id.replace(/-/g, ""));
-      });
-
-      new Promise((fnResolve, fnReject) => {
-        fnGetDocsFromSearch(aCardIds, fnResolve);
-      }).then((aOwnedCards) => {
-        fnDispatch(setCardSearch([aCards, aOwnedCards], false));
-        fnDispatch({
-          type: "SET_EXPANSION_FILTER",
-          payload: [],
-        });
-        fnHistory.push("/cards");
-      });
-    });
-  };
-
-  function getCardsFromExpansion(
-    aCardsFromExpansion,
-    currentURL,
-    fnResolveExpansions
-  ) {
-    fetch(currentURL)
-      .then((response) => response.json())
-      .then((data) => {
-        aCardsFromExpansion = aCardsFromExpansion.concat(data.data);
-        if (data.has_more) {
-          getCardsFromExpansion(
-            aCardsFromExpansion,
-            data.next_page,
-            fnResolveExpansions
-          );
-        } else {
-          fnResolveExpansions(aCardsFromExpansion);
-        }
-      });
-  }
-
-  const fnGetDocsFromSearch = (aCardIds, fnResolve) => {
-    if (oUser) {
-      //Firebase only allows for 10 comparison values, have to make multiple queries
-      let aBatchQueries = [];
-
-      while (aCardIds.length) {
-        let aChunkOfTenIds = aCardIds.splice(0, 10);
-
-        aBatchQueries.push(
-          new Promise((fnResolveQuery, fnRejectQuery) => {
-            getDocs(
-              query(
-                collection(db, oUser.uid),
-                where("sId", "in", aChunkOfTenIds)
-              )
-            ).then((docs) => {
-              let aDocsFetched = [];
-              docs.forEach((oCard) => {
-                aDocsFetched.push(oCard.data());
-              });
-              fnResolveQuery(aDocsFetched);
-            });
-          })
-        );
-      }
-
-      Promise.all(aBatchQueries).then((aRetrievedQueries) => {
-        let aDocsRetrieved = [];
-
-        aRetrievedQueries.forEach((aQuery) => {
-          aDocsRetrieved = aDocsRetrieved.concat(aQuery);
-        });
-
-        fnResolve(aDocsRetrieved);
-      });
-    } else {
-      fnResolve(null);
-    }
+    fnHistory.push("/cards");
   };
 
   return (
