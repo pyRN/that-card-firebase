0d41794030c1a0cf4ce513534fe953e66361ef4c
Jacob Mayeux <jacobmayeux@gmail.com>
2022-06-19 18:53:03 -0500

    Added random card to landing page, added sign up button, made landing page larger card that is responsive.

--- src/components/Main/MainContainer/MainContainer.jsx
+++ src/components/Main/MainContainer/MainContainer.jsx
@@ -1,19 +1,51 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./MainContainer.css";
 //Media
-import MTGCard from "../../../multimedia/Magic_card_back.jpg";
+import MtgCard from "../../../multimedia/Magic_card_back.jpg";
 
-export default function MainContainer() {
+const MainContainer = () => {
+  const fnHistory = useHistory();
+  const [oLandingMtgCard, fnSetLandingMtgCard] = useState(MtgCard);
+  const fnGetRandomCard = () => {
+    fetch(`https://api.scryfall.com/cards/random`)
+      .then((response) => response.json())
+      .then((oCardInfo) => {
+        const aImageSrc =
+          oCardInfo.card_faces && oCardInfo.card_faces[0].image_uris
+            ? oCardInfo.card_faces[0].image_uris.normal
+            : oCardInfo.image_uris.normal;
+        fnSetLandingMtgCard(aImageSrc);
+      });
+  };
+
   return (
-    <div className="page-container">
-      <div className="static-card">
-        <div className="card-items" align="center">
-          <h1 align="center">Do I Have That Card?</h1>
-
-          <img src={MTGCard} className="mtg-card" alt="Magic Card Back" />
-          <p align="center">
-            A simple Magic: The Gathering&#169; card collection manager
-          </p>
-        </div>
+    <div className="landing-card flex-row center w-100">
+      <div className="flex-column  landing-content w-50 center">
+        <h1 className="main-heading">Do I Have That Card?</h1>
+        <p className="sub-heading">
+          A light weight Magic: The Gathering&#169; card collection manager for
+          physical cards.
+        </p>
+        <button
+          className="btn btn-submit"
+          onClick={() => {
+            fnHistory.push("/signUp");
+          }}
+        >
+          Sign Up
+        </button>
+      </div>
+      <div className="random-card" onClick={fnGetRandomCard}>
+        <img
+          src={oLandingMtgCard}
+          className="mtg-card w-50"
+          alt="Magic Card Back"
+          id="landing-mtg-card"
+        />
       </div>
     </div>
   );
-}
+};
+
+export default MainContainer;
